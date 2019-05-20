4ac929682e615baf538a42fc7e59f7d01f91f680
roytechnik <mrinalroy1975@gmail.com>
2019-05-20 10:33:03 +0530

    modified the loop after the review

--- coding-challenges/challenge3/script.js
+++ coding-challenges/challenge3/script.js
@@ -1,9 +1,11 @@
 var arr1, arr2 = [];
+//Arrays have been assumed to be equal if their lengths are equal and
+// their elements are same (equal). It is assumed that arrays may not be
+// in same order and arrays are said to also equal if its lengths are equal
+// elements are equal, but may not be in same order.
 
 arr1 = ["a", "b", "c", "g", "p", "q", "r"];
 arr2 = ["g", "r", "p", "c", "a", "q", "b"];
-
-arrEqCompare(arr1, arr2);
 
 function arrEqCompare(array1, array2) {
     var arr1 = array1.sort();
@@ -12,14 +14,15 @@ function arrEqCompare(array1, array2) {
         var k = arr1.length;
         var eqArr = [];
         for (var i=0; i<arr1.length; ++i) {
-            for (var j=0; i<arr2.length; ++j) {
-                if (arr1[i] === arr2[j]) {
+                if (arr1[i] === arr2[i]) {
                     eqArr.push(1);
                 } continue;
             }
-        } if (eqArr.length === k) {
+        if (eqArr.length === k) {
             result = "Arrays are Equal";
         } else result = "Arrays are Not Equal";
     } else result = "Arrays are Not Equal";
 console.log(result);
 }
+
+arrEqCompare(arr1, arr2);
