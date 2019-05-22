bd9fa87b5c3337ec0b7fdb4bf7345832dd9c296c
roytechnik <mrinalroy1975@gmail.com>
2019-05-22 10:21:23 +0530

    coding challenge5 updated

--- coding-challenges/challenge5/script.js
+++ coding-challenges/challenge5/script.js
@@ -1,24 +1,17 @@
 function printPattern(arg) {
     var output;
-    count = 1;
     for (var i=1; i<=5; i++) {
         output = "";
         for (var j=1; j<=i; j++) {
             output += arg;
         } console.log(output);
-        count++;
-    } if (count>5) {
-        for (var i=5; i<5; i--) {
-            output = "";
-            for (var j=1; j<=i; j++) {
-                output += arg;
-            } console.log(output);
-        }
+    }
+    for (var i=4; i>=1; i--) {
+        output = "";
+        for (var j=1; j<=i; j++) {
+            output += arg;
+        } console.log(output);
     }
     }
 
-
-
-
-
-printPattern('A');
+printPattern('*');
