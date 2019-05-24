bbb118533932fe6e7603c0bb05fd540161fa8170
roytechnik <mrinalroy1975@gmail.com>
2019-05-24 15:19:45 +0530

    coding challenge6 corrected the json data parse method

--- coding-challenges/challenge6/script.js
+++ coding-challenges/challenge6/script.js
@@ -4,15 +4,15 @@ function citiesList(inputletter) {
         url: "https://raw.githubusercontent.com/attainu-falcon/attainu-falcon/master/coding-challenges/cities.json",
         datatype: "json",
         success: function(inputdata) {
+            var inputjson = JSON.parse(inputdata);
             var cityarray = [];
-            for (var i=0; i<inputdata.length; i++) {
-                if (inputletter === inputdata[i].name[0]) {
-                    cityarray.push(inputdata[i].name)
+            for (var i=0; i<inputjson.length; i++) {
+                if (inputletter === inputjson[i].name[0]) {
+                    cityarray.push(inputjson[i].name)
                 }
             }
-            console.log(cityarray);
-            // return cityarray;
 
+            console.log(cityarray);
 
 
         },
