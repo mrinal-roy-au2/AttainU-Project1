75cbfe813e3406eb6daec3ad6667ec183259f01b
roytechnik <mrinalroy1975@gmail.com>
2019-05-28 23:06:50 +0530

    Ver-03 Submission after all table properties displayed in output

--- coding-challenges/challenge8/script.js
+++ coding-challenges/challenge8/script.js
@@ -1,47 +1,33 @@
-
-$("button").click(function(inputLang) {
-    var inputLang = $("#language").val();
-    $.ajax({
-        url: "bookdata.json",
-        // url: "https://raw.githubusercontent.com/attainu-falcon/attainu-falcon/master/coding-challenges/data/books.json";
-        datatype: "json",
-        success: function(bookdata) {
-            // var inputjson = JSON.parse(inputdata);
-            var bookarray = [];
-            for (var i=0; i<bookdata.length; i++) {
-                if (bookdata[i].language.indexOf(inputLang) != -1) {
-                    bookarray.push(bookdata[i]);
-                }
-            }
-            printInTable(bookarray);
-            function printInTable(bookarray) {
-                var table = document.createElement('table');
-                document.querySelector('body').appendChild(table);
-                $('table').addClass("table", "table-striped");
-                for (var i=0; i<bookarray.length; ++i) {
-                        var tableRows = document.createElement('tr');
-                        table.appendChild(tableRows);
-                        var headerValues = ['title', 'author', 'country', 'language', 'link', 'pages', 'year'];
-                        for (var j=0; j<headerValues.length; ++j) {
-                            if (i<1 && j<headerValues.length) {
-                                    var header = document.createElement('th');
-                                    header.appendChild(document.createTextNode(headerValues[j]));
-                                    tableRows.appendChild(header);
-                                } else {
-                            var tableValues = document.createElement('td');
-                            tableValues.appendChild(document.createTextNode(bookarray[i][headerValues[j]]));
-                            tableRows.appendChild(tableValues);
-                        }
+$(document).ready(function(){
+    $("button").click(function() {
+        $('table').remove();
+        var inputLang = $("#language").val();
+        $.ajax({
+            url: "bookdata.json",
+            // url: "https://raw.githubusercontent.com/attainu-falcon/attainu-falcon/master/coding-challenges/data/books.json",
+            datatype: "json",
+            success: function(bookdata) {
+                // var inputjson = JSON.parse(inputdata);
+                var bookarray = [];
+                for (var i=0; i<bookdata.length; i++) {
+                    if (bookdata[i].language.indexOf(inputLang) != -1) {
+                        bookarray.push(bookdata[i]);
                     }
                 }
+                printInTable(bookarray);
+                function printInTable(bookarray) {
+                    $('body').append('<table><tr><th>Title</th><th>Author</th><th>Country</th><th>Language</th><th>Link</th><th>Pages</th><th>Year</th></tr></table>');
+                    $('table').addClass("table-container table table-striped");
+                    for (var i=0; i<bookarray.length; ++i) {
+                        $('table').append('<tr><td>'+bookarray[i].title+'</td><td>'+bookarray[i].author+'</td><td>'+bookarray[i].country+'</td><td>'+bookarray[i].language+'</td><td>'+bookarray[i].link+'</td><td>'+bookarray[i].pages+'</td><td>'+bookarray[i].year+'</td></tr>');
+                            }
+                        }
+                    },
+            statusCode: {
+                204: function() {
+                    alert("Book Not Found");
+                    }
             }
-
-            console.log(bookarray);
-        },
-        statusCode: {
-            404: function() {
-            alert("Book Not Found");
-            }
-        }
+        });
     });
 });
