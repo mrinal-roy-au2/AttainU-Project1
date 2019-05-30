f9c0db455bd3bb27b3b1a3172b1c8d1374f3dcc6
roytechnik <mrinalroy1975@gmail.com>
2019-05-30 11:39:59 +0530

    Improved the UI and submitting ver-02

--- coding-challenges/challenge10/script.js
+++ coding-challenges/challenge10/script.js
@@ -10,9 +10,11 @@ $(function() {
                 hammingLen = (arg1.charAt(i) != arg2.charAt(i)) ? (hammingLen+1) : hammingLen;
             $("form").append("<h3>Hamming Length of the 2 strings : "+hammingLen+"</h3>");
             $("h3").addClass("alert alert-success");
+            $("h3").css({"position":"relative","top":"10px"});
         } else {
             $("form").append("<h3>Both Strings should be of same length. Try Again!</h3>");
             $("h3").addClass("alert alert-danger");
+            $("h3").css({"position":"relative","top":"10px"});
         }
 });
 });
