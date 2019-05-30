ff15c547602b64269ca46791f70e436382f567c9
roytechnik <mrinalroy1975@gmail.com>
2019-05-30 11:27:05 +0530

    Added UI and submitting ver-01

--- coding-challenges/challenge10/script.js
+++ coding-challenges/challenge10/script.js
@@ -1,11 +1,18 @@
-hammingLength('karolin', 'kathrin');
-hammingLength('karolin', 'kerstin');
-hammingLength('1011101', '1001001');
-hammingLength('2173896', '2233796');
-
-function hammingLength(arg1, arg2) {
-    var hammingLen = 0;
-    for (var i=0; i<=arg1.length; i++)
-        commonLen = (arg1.charAt(i) != arg2.charAt(i)) ? (hammingLen+1) : hammingLen;
-    console.log(hammingLen);
-    }
+$(function() {
+    $("h3").remove();
+    $('button').click(function() {
+        $("h3").remove();
+        if ($("#string1").val().length === $("#string2").val().length) {
+            var arg1 = $("#string1").val();
+            var arg2 = $("#string2").val();
+            var hammingLen = 0;
+            for (var i=0; i<arg1.length; i++)
+                hammingLen = (arg1.charAt(i) != arg2.charAt(i)) ? (hammingLen+1) : hammingLen;
+            $("form").append("<h3>Hamming Length of the 2 strings : "+hammingLen+"</h3>");
+            $("h3").addClass("alert alert-success");
+        } else {
+            $("form").append("<h3>Both Strings should be of same length. Try Again!</h3>");
+            $("h3").addClass("alert alert-danger");
+        }
+});
+});
