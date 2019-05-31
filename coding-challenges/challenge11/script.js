2b733a1a8fcfe4b5afc1122ea045c11b1bc520a6
roytechnik <mrinalroy1975@gmail.com>
2019-05-31 12:00:08 +0530

    submitting corrected ver-01 of coding challenge11. Still some extra lines of output getting executed.

--- coding-challenges/challenge11/script.js
+++ coding-challenges/challenge11/script.js
@@ -3,31 +3,16 @@ $(document).ready(function(){
             url: "https://api.github.com/users",
             datatype: "json",
             success: function(userdata) {
+                $('body').append('<div class = "card"></div>');
                 for (var i=0; i<userdata.length; i++) {
+                    var avatar = userdata[i].avatar_url;
                     var username = userdata[i].login;
                     var userid = userdata[i].id;
-                    var avatar = userdata[i].avatar_url;
-                    // var userprofile = userdata.html_url;
-                    // var user_sub = userdata.subscriptions_url;
-                    // var user_org = userdata.organizations_url;
-                    // var followers = userdata.followers_url;
-                    // var following = userdata.following_url;
-                    $('body').append('<div class = "card"></div>');
-                    $('card').append('<img src = "'+avatar+' class="card-img-top" style="width: 18rem;">');
-                    // $('img').addClass("card-img-top");
-                    $('card-img-top').append('<div class="card-body"></div>');
-                    // $('div').addClass('card-body');
-                    $('card-body').append('<h5 class="card-title">'+username+'</h5>');
-                    // $('h5').addClass("card-title");
-                    $('card-title').append('<p class="card-text">'+userid+'</p>');
-                    // $('p').addClass("card-text");
-                    // //
-                    //
-                    // $('p').addClass("card-text");
-                    console.log(username);
-                    console.log(userid);
-                    }
-
+                    $('.card').append('<img src = "'+avatar+' class="card-img-top" style="width: 18rem;">');
+                    $('.card').append('<div class="card-body"></div>');
+                    $('.card-body').append('<h5 class="card-title">User: '+username+'</h5>');
+                    $('.card-body').append('<p class="card-text">ID: '+userid+'</p>');
+                }
                 },
             statusCode: { 204: function() {alert("User Not Found");} }
         });
