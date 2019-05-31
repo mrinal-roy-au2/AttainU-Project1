c3ef8d376b43717a7515c283367aad6f9a24b7f2
roytechnik <mrinalroy1975@gmail.com>
2019-05-31 12:25:08 +0530

    Submitting final version-02

--- coding-challenges/challenge11/script.js
+++ coding-challenges/challenge11/script.js
@@ -1,17 +1,15 @@
 $(document).ready(function(){
+
             $.ajax({
             url: "https://api.github.com/users",
             datatype: "json",
             success: function(userdata) {
-                $('body').append('<div class = "card"></div>');
                 for (var i=0; i<userdata.length; i++) {
-                    var avatar = userdata[i].avatar_url;
-                    var username = userdata[i].login;
-                    var userid = userdata[i].id;
-                    $('.card').append('<img src = "'+avatar+' class="card-img-top" style="width: 18rem;">');
-                    $('.card').append('<div class="card-body"></div>');
-                    $('.card-body').append('<h5 class="card-title">User: '+username+'</h5>');
-                    $('.card-body').append('<p class="card-text">ID: '+userid+'</p>');
+                    $('body').append('<div class = "card" style="width: 30rem;"><img src = "'+userdata[i].avatar_url+' class="card-img-top"><div class="card-body"><h5 class="card-title">User: '+userdata[i].login+'</h5><p class="card-text">ID: '+userdata[i].id+'</p></div></div>');
+                    // $('.card').append('<div class="card-body"></div>');
+                    // $('.card').append('<img src = "'+userdata[i].avatar_url+' class="card-img-top">');
+                    // $('.card-body').append('<h5 class="card-title">User: '+userdata[i].login+'</h5>');
+                    // $('.card-body').append('<p class="card-text">ID: '+userdata[i].id+'</p>');
                 }
                 },
             statusCode: { 204: function() {alert("User Not Found");} }
